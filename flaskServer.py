ec114242603cdc97b41ca3fcded98385ca229e16
Hasin Mahir <130781004+HasinMahir77@users.noreply.github.com>
2025-04-15 01:23:24 +0600

    port 3000

--- flaskServer.py
+++ flaskServer.py
@@ -92,4 +92,4 @@ def receive_sensor_data():
 
 if __name__ == '__main__':
     setup_database()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
